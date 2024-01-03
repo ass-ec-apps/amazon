bc980db6237fe17f0006f0298aeaa5d83c7e6423
Anil <anilkumar.l0920@gmail.com>
2024-01-03 17:24:55 +0000

    updated Deploy.sh

--- Deploy.sh
+++ Deploy.sh
@@ -1,1 +1,3 @@
-i am woking in some compnay has a devops engg and i am doing some deployment in the server 
+
+-----this is updaed lines-------
+i am woking in some compnay has a devops engg and i am doing some deployment in iithe server 
